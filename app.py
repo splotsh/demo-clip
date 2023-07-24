0b9e45c3879df1cf3e57ed55292c76b6aceb0458
Susanta Gautam <xuxant02@gmail.com>
2023-07-24 15:57:44 +0545

    debug

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def handler(context: dict, request: Request) -> Response:
     print(response)
 
     Response(
-        json=response["similarities"],
+        json=response["similarity"],
         status=200
     )
 
